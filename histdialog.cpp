dd6ba34410a7937ad94f5a49868ba45f6e0382fd
kgh2927845 <jupiter720@outlook.com>
2017-03-07 20:33:03 -0300

    new lines added

--- histdialog.cpp
+++ histdialog.cpp
@@ -44,4 +44,32 @@ HistDialog::on_done_clicked()
 }
 
 void
-
+HistDialog::on_ui_clear_clicked()
+{
+	int ret = QMessageBox::question(this, "Confirm clearing",
+				"This will erase all the wiping history done so far\n"
+				"Are you sure you want to clear ?",
+				QMessageBox::Yes,
+				QMessageBox::No | QMessageBox::Default);
+	if (ret != QMessageBox::Yes) {
+		return;
+	}
+	QSqlDatabase db;
+	db = QSqlDatabase::addDatabase("QSQLITE", "WIPEDB");
+	db.setDatabaseName(DBName);
+	if (!db.open()) {
+		hist->appendPlainText("Could not open history database");
+		return;
+	}
+	
+	QSqlQuery qy(db);
+	qy.prepare("delete from wipehistory");
+	qy.exec();
+	if (!qy.isActive()) {
+		QMessageBox::critical(this, "Database error", "Could not clear history database");
+		db.close();
+		return;
+	}
+	db.close();
+	hist->clear();
+}
